e2e2f5973dfd06a5546e0444ed75f39299ce29c1
PeraSite <pretocki3@gmail.com>
2023-04-15 13:37:42 +0900

    feat: Init

---
+++ Client/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+
+class Client {
+	static void Main() {
+		// Create TCP Client, Connect to server
+		using var client = new TcpClient();
+		client.Connect(IPAddress.Loopback, 8000);
+		Console.WriteLine("Server connected!");
+
+		// Create read, write stream
+		using var stream = client.GetStream();
+		using var reader = new BinaryReader(stream);
+		using var writer = new BinaryWriter(stream);
+
+		// Create RSA providers for server, client
+		using var serverRSA = new RSACryptoServiceProvider();
+		using var clientRSA = new RSACryptoServiceProvider();
+
+		// Read server's public key
+		{
+			var publicKeyLength = reader.ReadInt32();
+			var publicKeyBytes = reader.ReadBytes(publicKeyLength);
+			serverRSA.ImportRSAPublicKey(publicKeyBytes, out _);
+		}
+
+		// Send client's public key
+		{
+			var publicKeyBytes = clientRSA.ExportRSAPublicKey();
+			writer.Write(publicKeyBytes.Length);
+			writer.Write(publicKeyBytes);
+		}
+
+		while (true) {
+			// Read input from user
+			Console.Write("Input:");
+			var message = Console.ReadLine() ?? throw new Exception("Please input the message");
+			var messageBytes = Encoding.UTF8.GetBytes(message);
+
+			// Encrypt input using server's public key
+			var encryptedData = serverRSA.Encrypt(messageBytes, true);
+
+			// Send encrypted data with it's length to server
+			writer.Write(encryptedData.Length);
+			writer.Write(encryptedData);
+			Console.WriteLine($"Sent:{message}");
+
+			// Receive echo data
+			var receivedDataLength = reader.ReadInt32();
+			var receivedData = reader.ReadBytes(receivedDataLength);
+			var decryptedReceivedData = clientRSA.Decrypt(receivedData, true);
+			var decrypted = Encoding.UTF8.GetString(decryptedReceivedData);
+			Console.WriteLine($"Received:{decrypted}");
+		}
+	}
+}
---
+++ Server/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+
+class Server {
+	private static void Main() {
+		// Create TCP Server, listen on port 8000
+		var listener = new TcpListener(IPAddress.Any, 8000);
+		listener.Start();
+		Console.WriteLine("Server started!");
+
+		while (true) {
+			// Accept client connection
+			using var client = listener.AcceptTcpClient();
+			Console.WriteLine("Client connected!");
+
+			// Create read/write streams
+			using var stream = client.GetStream();
+			using var reader = new BinaryReader(stream);
+			using var writer = new BinaryWriter(stream);
+
+			// Create RSA providers for client and server
+			using RSACryptoServiceProvider clientRSA = new RSACryptoServiceProvider();
+			using RSACryptoServiceProvider serverRSA = new RSACryptoServiceProvider();
+
+			// Send server's public key to client
+			{
+				var publicKeyBytes = serverRSA.ExportRSAPublicKey();
+				writer.Write(publicKeyBytes.Length);
+				writer.Write(publicKeyBytes);
+			}
+
+			// Receive client's public key
+			{
+				var publicKeyLength = reader.ReadInt32();
+				var publicKeyBytes = reader.ReadBytes(publicKeyLength);
+				clientRSA.ImportRSAPublicKey(publicKeyBytes, out _);
+			}
+
+			while (client.Connected) {
+				// Receive encrypted data from client
+				var encryptedDataLength = reader.ReadInt32();
+				var encryptedData = reader.ReadBytes(encryptedDataLength);
+
+				// Decrypt data using server's private key
+				var decryptedData = serverRSA.Decrypt(encryptedData, true);
+				var message = Encoding.UTF8.GetString(decryptedData);
+				Console.WriteLine("Received: " + message);
+
+				// Echo message back to client
+				var encryptedMessage = clientRSA.Encrypt(Encoding.UTF8.GetBytes($"ECHO_{message}"), true);
+				writer.Write(encryptedMessage.Length);
+				writer.Write(encryptedMessage);
+			}
+		}
+	}
+}
